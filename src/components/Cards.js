dd918f39e6b4ceff137bcd0c7ad303b6f0ac0ede
Ayan <ayanvahmad6392@gmail.com>
2024-02-28 20:24:57 +0530

    add commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -27,7 +27,7 @@ function Cards({courses,category}){
 
 
     return(
-        <div className='flex flex-wrap justify-center gap-4 mb-4'>
+        <div className='flex flex-wrap justify-center gap-4 mb-10'>
             {
             getCourses().map((course) => {
                 return <Card  key={course.id} course={course} likedCourses={likedCourses}
