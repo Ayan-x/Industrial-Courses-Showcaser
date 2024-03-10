15f97c8807091bd5d23c3025f5a0c534d730e8f8
Ayan <ayanvahmad6392@gmail.com>
2024-03-10 23:44:11 +0530

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NavBar(){
     return(
         <div>
-            <nav className='bg-bgDark py-4'>
+            <nav className='bg-bgDark py-3'>
             <h1 className='text-3xl font-bold text-center
             text-white '>Top Notch Courses</h1>
             </nav>
